a09112725e1ad29964fc91f5386344366f67501f
Nicolay Byvshev <nic.law.ab@gmail.com>
2024-04-14 23:18:09 +0700

    set js client side validation

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const inputValue = document.getElementById("email");
+const inputEmail = document.getElementById("email");
 const button = document.getElementById("button");
-const invalidMassage = document.getElementById("invalid-massage"); 
+const invalidMassage = document.getElementById("invalid-massage");
+const container = document.querySelector(".container"); 
 
 button.addEventListener("click", () => {
-    console.log(inputValue.value);
+    validateInput();
 });
+
+function validateInput() {
+    if (inputEmail.validity.typeMismatch === true || inputEmail.value === ""){
+        inputEmail.setCustomValidity("Valid email required");
+        invalidMassage.style.display = "inline";
+        inputEmail.style.border = "1px solid red";
+    } else {
+        inputEmail.style.border = "1px solid var(--Grey)";
+        container.remove();
+    }
+}
